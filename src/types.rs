614746ff26f10741fea57931824e2746347a5915
Nathan Ringo <remexre@gmail.com>
2019-06-21 22:01:41 -0500

    ...

--- src/types.rs
+++ src/types.rs
@@ -5,6 +5,35 @@ use rand::{thread_rng, Rng};
 #[derive(Clone, Copy, Debug, Display, Eq, From, Hash, Into, Ord, PartialEq, PartialOrd)]
 pub struct MessageTypeID(u64);
 
+impl MessageTypeID {
+    /// The [DebugPrint](https://remexre.xyz/stahlos/protocols/debug.md#debugprint) message type.
+    pub const DEBUG_PRINT: MessageTypeID = MessageTypeID(0x66c379ab0b7196ef);
+
+    /// The [Ping](https://remexre.xyz/stahlos/protocols/debug.md#ping) message type.
+    pub const PING: MessageTypeID = MessageTypeID(0x0000000000000000);
+
+    /// The [Pong](https://remexre.xyz/stahlos/protocols/debug.md#pong) message type.
+    pub const PONG: MessageTypeID = MessageTypeID(0xffffffffffffffff);
+
+    /// The [ReadBytesData](https://remexre.xyz/stahlos/protocols/byte-input-stream.md#readbytesdata) message type.
+    pub const READ_BYTES_DATA: MessageTypeID = MessageTypeID(0x9d373588abfc316c);
+
+    /// The [ReadBytes](https://remexre.xyz/stahlos/protocols/byte-input-stream.md#readbytes) message type.
+    pub const READ_BYTES: MessageTypeID = MessageTypeID(0x453fbbee6bd6a904);
+
+    /// The [ReadEOF](https://remexre.xyz/stahlos/protocols/byte-input-stream.md#readeof) message type.
+    pub const READ_EOF: MessageTypeID = MessageTypeID(0x66822efd36030ee1);
+
+    /// The [WriteBytesDone](https://remexre.xyz/stahlos/protocols/byte-output-stream.md#writebytesdone) message type.
+    pub const WRITE_BYTES_DONE: MessageTypeID = MessageTypeID(0xfa2df296436c000b);
+
+    /// The [WriteBytes](https://remexre.xyz/stahlos/protocols/byte-output-stream.md#writebytes) message type.
+    pub const WRITE_BYTES: MessageTypeID = MessageTypeID(0xa80817401471655f);
+
+    /// The [WriteEOF](https://remexre.xyz/stahlos/protocols/byte-output-stream.md#writeeof) message type.
+    pub const WRITE_EOF: MessageTypeID = MessageTypeID(0xd43aa3d9caaf192e);
+}
+
 /// The ID of a task.
 #[derive(Clone, Copy, Debug, Display, Eq, From, Hash, Into, Ord, PartialEq, PartialOrd)]
 pub struct TaskID(u64);
